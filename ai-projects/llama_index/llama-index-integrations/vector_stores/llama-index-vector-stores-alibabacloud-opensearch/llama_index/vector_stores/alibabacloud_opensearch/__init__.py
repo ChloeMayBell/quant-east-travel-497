280a71ea6b5651575b9ed0427e43a176a252503c
Carter John <saraeva16@mp68.com>
2026-09-01 12:54:07 +0200

    Create __init__.py

---
+++ ai-projects/llama_index/llama-index-integrations/vector_stores/llama-index-vector-stores-alibabacloud-opensearch/llama_index/vector_stores/alibabacloud_opensearch/__init__.py
@@ -0,0 +1,12 @@
+from llama_index.vector_stores.alibabacloud_opensearch.base import (
+
+    AlibabaCloudOpenSearchConfig,
+
+    AlibabaCloudOpenSearchStore,
+
+)
+
+
+
+__all__ = ["AlibabaCloudOpenSearchConfig", "AlibabaCloudOpenSearchStore"]
+
